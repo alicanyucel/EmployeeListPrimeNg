200f49ed253cd866e806cb4db6d82c565a31ad8d
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-03-31 15:08:39 +0300

    Primeng EmployeeList

---
+++ src/modules/emploee/emploee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emploee',
+  templateUrl: './emploee.component.html',
+  styleUrls: ['./emploee.component.css']
+})
+export class EmploeeComponent {
+
+}
---
+++ src/modules/emploee/emploee.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmploeeComponent } from './emploee.component';
+
+
+
+@NgModule({
+  declarations: [
+    EmploeeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class EmploeeModule { }
