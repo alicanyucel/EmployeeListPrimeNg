a093ad4c500af21ea7adaf90b5dc7eabc8ab6ed9
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-04-02 15:55:48 +0300

    deneme

--- src/interfaces/employee.ts
+++ src/interfaces/employee.ts
@@ -1,2 +1,12 @@
 export interface Employee {
+    id:string;
+    firstName:string;
+    lastName:string;
+    dob:string;
+    email:string;
+    gender:string;
+    country:string;
+    role:string;
+    dateOfjoining:string;
+    active:boolean;
 }
--- src/services/employee.service.ts
+++ src/services/employee.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http'
+import { Employee } from 'src/interfaces/employee';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  private baseUrl='http://localhost:3000';
+
+  constructor(private http:HttpClient) { }
+  getEmployees():Observable<Employee[]>{
+    return this.http.get<Employee[]>(`${this.baseUrl}/employees`);
+  }
 }
